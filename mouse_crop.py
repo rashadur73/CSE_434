d8728da1f20cf82b8947323a3e5f534f8db65bcf
rashadur73 <rsdrcse14@gmail.com>
2019-06-21 23:16:35 +0600

    mouse croping function

---
+++ mouse_crop.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+cropping = False
+ 
+x_start, y_start, x_end, y_end = 0, 0, 0, 0
+ 
+image = cv2.imread('test2.jpg')
+oriImage = image.copy()
+ 
+ 
+def mouse_crop(event, x, y, flags, param):
+    # grab references to the global variables
+    global x_start, y_start, x_end, y_end, cropping
+ 
+    # if the left mouse button was DOWN, start RECORDING
+    # (x, y) coordinates and indicate that cropping is being
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x_start, y_start, x_end, y_end = x, y, x, y
+        cropping = True
+ 
+    # Mouse is Moving
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if cropping == True:
+            x_end, y_end = x, y
+ 
+    # if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates
+        x_end, y_end = x, y
+        cropping = False # cropping is finished
+ 
+        refPoint = [(x_start, y_start), (x_end, y_end)]
+ 
+        if len(refPoint) == 2: #when two points were found
+            roi = oriImage[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
+            cv2.imshow("Cropped", roi)
